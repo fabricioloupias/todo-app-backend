a0f24e0c134296e22864fdc909fba9bc1c575931
Fabricio Loupias <loupias.fabri@gmail.com>
2020-01-22 16:45:31 -0300

    remove folder of branch

--- dist/server.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-var __importDefault = (this && this.__importDefault) || function (mod) {
-    return (mod && mod.__esModule) ? mod : { "default": mod };
-};
-Object.defineProperty(exports, "__esModule", { value: true });
-const app_1 = __importDefault(require("./app"));
-const todoApi_constants_1 = require("./constants/todoApi.constants");
-app_1.default.listen(todoApi_constants_1.PORT, () => console.log(`Listening on port ${todoApi_constants_1.PORT}`));
