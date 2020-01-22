0435d17027114e1765631cd947ad87f43dbcac88
Fabricio Loupias <loupias.fabri@gmail.com>
2020-01-22 11:49:33 -0300

    config PORT

--- dist/constants/todoApi.constants.js
+++ dist/constants/todoApi.constants.js
@@ -5,5 +5,5 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 Object.defineProperty(exports, "__esModule", { value: true });
 const dotenv_1 = __importDefault(require("dotenv"));
 dotenv_1.default.config();
-exports.PORT = 9001;
+exports.PORT = process.env.PORT || 5000;
 exports.MONGODB_URI = `${process.env.MONGODB_URI}`;
